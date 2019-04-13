ec92fac2b82b648136d62d4f975763b67d43191a
hcgustavo <ghcaixeta@gmail.com>
2019-04-13 01:27:28 -0300

    [IMP] onNewUserCreated + onNewAudioUploaded

--- functions/index.js
+++ functions/index.js
@@ -4,3 +4,19 @@ const functions = require('firebase-functions');
 // The Firebase Admin SDK to access the Firebase Realtime Database.
 const admin = require('firebase-admin');
 admin.initializeApp();
+
+/**
+ * Triggered wher a new user is created
+ * Action: creates the user's register on Firestore
+ */
+exports.onNewUserCreated = functions.auth.user().onCreate((user) => {
+    
+});
+
+/**
+ * Triggered when new a new object is created in the bucket
+ * Action: create object's register on Firestore (it can be a question or an user's recorded answer)
+ */
+exports.onNewAudioUploaded = functions.storage.object().onFinalize((obj) => {
+    console.log("Audio id = " + obj.id);
+});
