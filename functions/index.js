bfe696812125931d34f897957f61c9f4b5db724e
hcgustavo <ghcaixeta@gmail.com>
2019-04-17 22:11:05 -0300

    [IMP] Mapping user answer file on Firestore

--- functions/index.js
+++ functions/index.js
@@ -32,18 +32,46 @@ exports.onDeletedUser = functions.auth.user().onDelete((user) => {
  * Action: create object's register on Firestore (it can be a question or an user's recorded answer)
  */
 exports.onNewAudioUploaded = functions.storage.object().onFinalize((obj) => {
-    // If it was uploaded on the questions folder, so it's an audio and it should be
-    // saved on the question collection
+    // If it was uploaded on the questions folder, so it's an audio for the question and a reference
+    // to it should be registered on the questions collection on Firestore
     if(obj.name.includes("questions/")) {
         admin.storage().bucket().file(obj.name).getSignedUrl({
             action: 'read',
             expires: '03-09-2100'
         })
         .then(signedUrls => {
-            admin.firestore().collection("questions").doc().create({
+            return admin.firestore().collection('questions').doc().create({
                 name: obj.name,
                 downloadUrl: signedUrls[0]
             })
+        })
+        .then(documentData => {
+            console.log("Saved question on Firestore");
+        })
+        .catch(error => {
+            console.error("Error saving question on Firestore: " + error);
+        })
+    }
+    // Otherwise, it's an user's answer file and a reference to it should be registered on the
+    // users collection on Firestore
+    else {
+        admin.storage().bucket().file(obj.name).getSignedUrl({
+            action: 'read',
+            expires: '03-09-2100'
+        })
+        .then(signedUrls => {
+            return admin.firestore().collection('users').doc(obj.metadata.customMetadata.user_uid).update({
+                answers: FirebaseFirestore.FieldValue.arrayUnion({
+                    questionId: obj.metadata.customMetadata.question_id,
+                    downloadUrl: signedUrls[0]
+                })
+            })
+        })
+        .then(result => {
+            console.log("Saved user's answer on Firestore")
+        })
+        .catch(error => {
+            console.error("Error saving user's answer on Firestore: " + error);
         })
     }
 });
